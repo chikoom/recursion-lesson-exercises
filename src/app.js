ad9607b0d6294bd203aec0293a0fde624cb1db9a
Chikoom <callforcopy@gmail.com>
2020-08-02 12:20:38 +0300

    recursions

--- src/app.js
+++ src/app.js
@@ -4,24 +4,26 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
-
+const findFactorial = function(number) {
+  if(number === 1) return 1
+  return number * findFactorial(--number)
 }
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
-
+const reverseString = function(str) {
+  if (str.length === 1) return str
+  return str.charAt(str.length-1) + reverseString(str.substring(0,str.length-1))
 }
+
 
 //Exercise 3
 const arr1 = [1, 2, 3]
 const arr2 = []
 
-const swap = function() {
-  //Your code here
-
+const swap = function(arr1, arr2) {
+  if(arr1.length===0)return
+  arr2.push(arr1.shift())
+  swap(arr1, arr2)
 }
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
